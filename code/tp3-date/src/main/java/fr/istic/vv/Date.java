303794a1f6c633601f20511d00ced8485b60582d
mfouillen <mathis.fouillen@etudiant.univ-rennes.fr>
2023-12-11 10:10:48 +0100

    Implemented Dzte

--- code/tp3-date/src/main/java/fr/istic/vv/Date.java
+++ code/tp3-date/src/main/java/fr/istic/vv/Date.java
@@ -2,16 +2,96 @@ package fr.istic.vv;
 
 class Date implements Comparable<Date> {
 
-    public Date(int day, int month, int year) { }
+    private int day;
+    private int month;
+    private int year;
 
-    public static boolean isValidDate(int day, int month, int year) { return false; }
+    public Date(int day, int month, int year) { 
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
 
-    public static boolean isLeapYear(int year) { return false; }
+    public static boolean isValidDate(int day, int month, int year) {
+        if (day <= 0){
+            return false;
+        }
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return day <= 31;
+            case 2:
+                boolean isValid = isLeapYear(year) ? day <= 29 : day <= 28;
+                return isValid;
+            case 4:
+            case 6:
+            case 9:
+                return day <= 30;
+            default:
+                return false;
+        }
+    }
 
-    public Date nextDate() { return null; }
+    public static boolean isLeapYear(int year) { 
+        return ((year%4 == 0 && year%100 == 0) || year%400 == 0);
+    }
 
-    public Date previousDate() { return null; }
+    public Date nextDate() { 
+        if(isValidDate(this.day+1, this.month, this.year)){
+            return new Date(this.day+1, this.month, this.year);
+        }
+        else if(isValidDate(this.day, this.month+1, this.year)) {
+            return new Date(this.day, this.month+1, this.year);
+        }
+        else {
+            return new Date(this.day, this.month, this.year+1);
+        }
+     }
 
-    public int compareTo(Date other) { return 0; }
+    public Date previousDate() {
+        if(this.day != 1){
+            return new Date(this.day-1, this.month, this.year);
+        }
+        else if(this.month != 1){
+            return new Date(1, this.month-1, this.year);
+        }
+        else {
+            return new Date(1, 1, this.year-1);
+        }
+    }
+
+    public int compareTo(Date other) {
+
+        // year comparaison
+        if(this.year > other.year){
+            return 1;
+        }
+        else if(this.year < other.year) {
+            return -1;
+        }
+
+        // month comparaison
+        if(this.month > other.month){
+            return 1;
+        }
+        else if(this.month < other.month) {
+            return -1;
+        }
+
+        // day comparaison
+        if(this.day > other.day){
+            return 1;
+        }
+        else if(this.day < other.day) {
+            return -1;
+        }
+
+        return 0; 
+    }
 
 }
